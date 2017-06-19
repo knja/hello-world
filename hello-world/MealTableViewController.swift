ef60553b1edddef7c6e63d3bec0dab00e63f0772
knja <s.knebel@gmail.com>
2017-06-19 17:23:03 +0200

    Tutorial abgeschlossen

--- hello-world/MealTableViewController.swift
+++ hello-world/MealTableViewController.swift
@@ -5,7 +5,7 @@
 //  Created by siarhei on 12.06.17.
 //  Copyright © 2017 Siarhei Knebeleu. All rights reserved.
 //
-
+import os.log
 import UIKit
 
 class MealTableViewController: UITableViewController {
@@ -15,15 +15,19 @@ class MealTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // call method
-        loadSampleMeals()
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+        // Use the edit button item provided by the table view controller.
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+        // Load any saved meals, otherwise load sample data.
+        if let savedMeals = loadMeals() {
+            meals += savedMeals
+        }
+        else {
+            // Load the sample data.
+            loadSampleMeals()
+        }
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,9 +51,9 @@ class MealTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         // Table view cells are reused and should be dequeued using a cell identifier.
-        let cellidentifier = "MealTableViewCell"
+        let cellIdentifier = "MealTableViewCell"
         
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellidentifier, for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
             as? MealTableViewCell else {
                 fatalError("The dequeued cell is not an instance of MealTableViewCell.")
         }
@@ -65,25 +69,27 @@ class MealTableViewController: UITableViewController {
     }
  
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            meals.remove(at: indexPath.row)
+            saveMeals()
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
@@ -100,15 +106,64 @@ class MealTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    super.prepare(for: segue, sender: sender)
+        switch (segue.identifier ?? "") {
+        case "AddItem":
+            os_log("Adding a new meal.", log: OSLog.default, type: .debug)
+        case "ShowDetail":
+            guard let mealDetailViewController = segue.destination as? MealViewController
+                else {
+                    fatalError("Unexpected destination: \(segue.destination)")
+            }
+            
+            guard let selectedMealCell = sender as? MealTableViewCell else{
+                fatalError("Unexpected sender: \(sender)")
+            }
+            
+            guard let indexPath = tableView.indexPath(for: selectedMealCell) else{
+                fatalError("The selected cell is not being displayed by the table")
+            }
+            
+            let selectedMeal = meals[indexPath.row]
+            
+            mealDetailViewController.meal = selectedMeal
+        
+        default:
+            fatalError("Unexpected Segue Identifier; \(segue.identifier)")
+        }
     }
-    */
+    
+    
+    
+    //MARK: Actions
+    @IBAction func unwindToMealList(sender: UIStoryboardSegue){
+        
+        
+        if let sourceViewController = sender.source as?
+            MealViewController, let meal = sourceViewController.meal{
+            
+            if let selectedIndexPath = tableView.indexPathForSelectedRow{
+                meals[selectedIndexPath.row] = meal
+                tableView.reloadRows(at: [selectedIndexPath], with: .none)
+            }
+            else{
+                // Add a new meal
+                let newIndexPath = IndexPath(row: meals.count, section: 0)
+                
+                meals.append(meal)
+                tableView.insertRows(at: [newIndexPath], with: .automatic)
+                
+            }
+            saveMeals()
+        }
+    }
+    
     //MARK: private Methods
     private func loadSampleMeals (){
         
@@ -132,5 +187,18 @@ class MealTableViewController: UITableViewController {
         meals += [meal1, meal2, meal3]
         
     }
-
+    
+    private func saveMeals() {
+        let isSuccsessfulSave = NSKeyedArchiver.archiveRootObject(meals, toFile: Meal.ArchiveURL.path)
+        if isSuccsessfulSave {
+            os_log("Meals successful saved", log: OSLog.default, type: .debug)} else{
+                os_log("failed to save", log: OSLog.default, type: .error)
+            
+            }
+    }
+    
+    private func loadMeals () -> [Meal]?{
+        return NSKeyedUnarchiver.unarchiveObject(withFile: Meal.ArchiveURL.path) as?
+            [Meal]
+    }
 }
