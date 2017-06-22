43c68a13c6723d3a810f9a1455a4083d3189da72
knja <s.knebel@gmail.com>
2017-06-22 19:47:44 +0200

    test scheme ausgabe

--- move.sh
+++ move.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
 prefix=temp
-suffix=$(date +%Y-%m-%d)  # The "+%s" option to 'date' is GNU-specific.
+suffix=$(date +%Y-%m-%d) 
 filename=$prefix.$suffix
-echo "das ist $SCHEME"
+echo "das ist $SchemeName"
 
 mv $BUILD_SOURCESDIRECTORY/output/$SDK/$CONFIGURATION/_XcodeTaskExport_hello-world/*.ipa $BUILD_SOURCESDIRECTORY/output/$SDK/$CONFIGURATION/$filename.ipa
-# #!/bin/bash
-# echo "Hello World"
-# echo "AGENT_WORKFOLDER is $AGENT_WORKFOLDER"
-# echo "AGENT_WORKFOLDER contents:"
-# ls -1 $AGENT_WORKFOLDER
-# echo "AGENT_BUILDDIRECTORY is $AGENT_BUILDDIRECTORY"
-# echo "AGENT_BUILDDIRECTORY contents:"
-# ls -1 $AGENT_BUILDDIRECTORY
-# echo "BUILD_SOURCESDIRECTORY is $BUILD_SOURCESDIRECTORY"
-# echo "BUILD_SOURCESDIRECTORY contents:"
-# ls -1 $BUILD_SOURCESDIRECTORY
-# echo "Over and out."
