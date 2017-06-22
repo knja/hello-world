5ed53d3829daa0fce768e7473575bf57712e2609
knja <s.knebel@gmail.com>
2017-06-22 19:06:30 +0200

    Pfad testen für shell Script

--- move.sh
+++ move.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-mv output/$(SDK)/$(Configuration)/_XcodeTaskExport_hello-world/*.ipa output/$(SDK)/$(Configuration)/_XcodeTaskExport_hello-world/new.ipa
+# mv output/$(SDK)/$(Configuration)/_XcodeTaskExport_hello-world/*.ipa output/$(SDK)/$(Configuration)/_XcodeTaskExport_hello-world/new.ipa
+#!/bin/bash
+echo "Hello World"
+echo "AGENT_WORKFOLDER is $AGENT_WORKFOLDER"
+echo "AGENT_WORKFOLDER contents:"
+ls -1 $AGENT_WORKFOLDER
+echo "AGENT_BUILDDIRECTORY is $AGENT_BUILDDIRECTORY"
+echo "AGENT_BUILDDIRECTORY contents:"
+ls -1 $AGENT_BUILDDIRECTORY
+echo "BUILD_SOURCESDIRECTORY is $BUILD_SOURCESDIRECTORY"
+echo "BUILD_SOURCESDIRECTORY contents:"
+ls -1 $BUILD_SOURCESDIRECTORY
+echo "Over and out."
