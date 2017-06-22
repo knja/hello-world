6a228759fd304f09e8bb1f6b43190d6e93424830
knja <s.knebel@gmail.com>
2017-06-22 19:56:18 +0200

    ipa name korrigiert

--- move.sh
+++ move.sh
@@ -2,7 +2,7 @@
 
 prefix=$SCHEMENAME
 suffix=$(date +%Y-%m-%d) 
-filename=$prefix.$suffix
+filename=$prefix_$suffix
 
 
 mv $BUILD_SOURCESDIRECTORY/output/$SDK/$CONFIGURATION/_XcodeTaskExport_hello-world/*.ipa $BUILD_SOURCESDIRECTORY/output/$SDK/$CONFIGURATION/$filename.ipa
